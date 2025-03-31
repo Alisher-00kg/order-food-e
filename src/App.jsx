14a0873b7324a124cd1426bee2b79ad69c3618ec
elmir <elmirbek2000kg>
2025-04-01 01:16:10 +0300

    saved:

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import Modal from "./components/Modal";
 import { useContext } from "react";
 import { ModalContex } from "./context/ContextModal";
 
-import React from "react";
-
 export const App = () => {
   return <div>App</div>;
 };
