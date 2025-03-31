1e7b3554b402e687ba9747030b8c20df93957919
Aiganysh <dzhumagulova8@gmail.com>
2025-04-01 01:06:55 +0300

    remove component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-import { MainContent } from "./components/MainContent";
 
 const App = () => {
-  return (
-    <div>
-      <MainContent />
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
