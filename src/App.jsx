bae9e5c433c7bc76e32cde2fc0584e702fa39cee
Beksultan <ergeshov7356@gmail.com>
2025-04-01 09:24:50 +0300

    create card list

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import React from "react";
-import { Button } from "./components/ui/Button";
+
+import { Card } from "./components/Card";
 const App = () => {
   return (
-    <div
-      style={{
-        background: "black",
-      }}
-    >
-      App
+    <div>
+      <Card
+        title={"Sushi"}
+        structure={"Finest fish and veggies"}
+        price={22.99}
+      />{" "}
+      <Card
+        title={"Schnitzel"}
+        structure={"A german speciality"}
+        price={16.0}
+      />{" "}
+      <Card
+        title={"Barbecue Burger"}
+        structure={"American ,.raw meaty"}
+        price={12.99}
+      />{" "}
+      <Card
+        title={"Green Bowl"}
+        structure={"Healthy...and green..."}
+        price={19.99}
+      />
     </div>
   );
 };
