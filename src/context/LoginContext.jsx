2f8ec99a70182b52fd7de30c374288f0d7aaaeb2
Daniel <danielamirshaev@gmail.com>
2025-04-01 19:35:15 +0300

    save changes

---
+++ src/context/LoginContext.jsx
@@ -0,0 +1,9 @@
+import React, { createContext } from "react";
+
+
+export const LoginContext = createContext();
+
+
+export const LoginProvider = ({ children }) => {
+  return <LoginContext.Provider>{children}</LoginContext.Provider>;
+};
