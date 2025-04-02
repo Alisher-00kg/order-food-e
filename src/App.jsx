63e1beabb7f1cde40e96892325e58246b82ba175
Alisher-00kg <alisher.estebesov@gmail.com>
2025-04-02 21:23:27 +0300

    fixed app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
 import React from "react";
-import { Header } from "./components/Header";
-import { MainContent } from "./components/MainContent";
 const App = () => {
-  return (
-    <div>
-      <Header />
-      <MainContent />
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
